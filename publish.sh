255e3aac6d99fa70e232426c7134f6789904cd90
bjrn <bjrn>
2020-08-22 16:57:56 +0200

    automatic publishing to publication branch

--- publish.sh
+++ publish.sh
@@ -7,7 +7,8 @@ echo "Delete everything except APK"
 find . | grep -v "Badminton-Umpire-Helper.apk" | grep -v "./.git" | xargs rm -rf
 echo "Add commit and push APK"
 git add -f Badminton-Umpire-Helper.apk
+git add -A
 git config --global user.email bjrn
 git config --global user.name bjrn
 git commit -m "New Badminton Umpire Helper Publication"
-# git push
+git push
