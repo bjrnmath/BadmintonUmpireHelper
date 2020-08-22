7b251140a08e8900b8808db485f84b3db37e2e9a
bjrn <bjrn>
2020-08-22 16:54:19 +0200

    tryout of automatic publishing to publication branch

--- publish.sh
+++ publish.sh
@@ -4,8 +4,10 @@ git checkout publication
 echo "Copy APK to main folder"
 cp app/build/outputs/apk/release/app-release.apk Badminton-Umpire-Helper.apk
 echo "Delete everything except APK"
-# find . | grep -v "Badminton-Umpire-Helper.apk" | grep -v "./.git" | xargs rm -rf
+find . | grep -v "Badminton-Umpire-Helper.apk" | grep -v "./.git" | xargs rm -rf
 echo "Add commit and push APK"
-git add Badminton-Umpire-Helper.apk
+git add -f Badminton-Umpire-Helper.apk
+git config --global user.email bjrn
+git config --global user.name bjrn
 git commit -m "New Badminton Umpire Helper Publication"
 # git push
